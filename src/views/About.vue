a58b43684d5ada55a4eec61f713e2e9c51456922
dlcincluded <carydavid85@gmail.com>
2021-10-24 22:36:02 -0400

    Basic Updates
    Putting basic info in Home and About pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,28 +6,33 @@
 			</div>
 			<div class="card-body">		
 				<div class="">	
-					<p>This is a list app I put together for myself, but you are free to use it if you'd like.</p>
-
+					<p>This is a list app I put together for myself, but you are free to use it if you'd like but please note that it is a beta project, and could change or break at any point.</p>
+					<br/>
 					<h3>Basic Features</h3>
 					<ul style="margin-left:2em">
+						<li>Account Creation/Registration (done)</li>
+						<li>Password Reset / Forgot Password (done)</li>
 						<li>New Lists</li>
 						<li>New Todos</li>
 							<ul style="margin-left:2em">
 								<li>Non-repeating</li>
-								<li>Weekly(WIP)</li>
+								<li>Daily(Should Work)</li>
+								<li>Weekly(Should Work)</li>
 								<li>Monthly(WIP)</li>
 								<li>More possibly in the future</li>
 							</ul>
-						<li>"Stay on page" setting for new todo(multi entry)</li>
-						<li>Auto Refresh(5sec) - as close as possible to "live" updates</li>
-						<li>Delete List/Todo</li>
-						<li>Friend System</li>
+						<li>Delete All Completed ToDos (done)</li>
+						<li>"Stay on page" setting for new todo (multi entry)</li>
+						<li>Auto Refresh(5sec) - as close as possible to "live" updates as I care to add</li>
+						<li>Delete List/Todo (done)</li>
+						<li>Friend System (WIP)</li>
 							<ul style="margin-left:2em">
-								<li>These features are still WIP</li>
-								<li>Send request/accept request</li>
-								<li>Send friend an invite to edit/view your list</li>
+								<li>Send request/accept request (done)</li>
+								<li>Delete Friend / Remove Friend request (done)</li>
+								<li>Send friend an invite to view & edit your list (done)</li>
+								<li>Permissions (WIP)</li>
 							</ul>
-						<li>Organize lists/todos by drag and dropping</li>
+						<li>Organize lists/todos by drag and dropping (done)</li>
 							<ul style="margin-left:2em">
 								<li>FYI On mobile you have to hold for at least .2sec before dragging</li>
 							</ul>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,25 @@
 			<div class="card-head" >
 				<h1>Home</h1>
 			</div>
-			<div class="card-body">		
-				<div class="list-item cursor-pointer" @click="goTo('/lists')">	
+			<div class="card-body">	
+				<h2>Welcome</h2>
+				<p>
+					This is a ToDo App I created for myself. But please feel free to use it if you would like. You can find more information <router-link to="/about">here</router-link>
+				</p>
+				<br/>
+				<p>
+					Please Note: I am actively developing this app, so please know there will be changes / updates / downtime as I go. Please do not rely on this as your sole source of anything for important tasks at this time. This is an ever changing app and is still in Beta
+				</p>
+				<br/>
+				<p>
+					Please click the icon in the bottom right to go to your lists, or the bottom left to go to your friends page. There is also the menu in the top left. 
+				</p>
+				<!-- <div class="list-item cursor-pointer" @click="goTo('/lists')">	
 					Click here to go to the lists<br>
 				</div>
 				<div class="list-item cursor-pointer" @click="goTo('/about')">	
 					Click here for more info
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>
